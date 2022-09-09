2f121e1d09ea82d77db3d7842fa0064ac005ace4
divineabagolu <abagoludivine@yahoo.com>
2022-09-09 07:59:11 -0700

    heree

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - entry point
- *
- * return: Always 0 (success)
- */
+* main - Entry point
+* Return: Always 0 (success)
+*
+*/
 int main(void)
 {
-		puts("\"programming is like building a mutilingual puzzle")
-		return(0);
+		putsi("\"programming is like building a mutilingual puzzle");
+		return (0);
 }
