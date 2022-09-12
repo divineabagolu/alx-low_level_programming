8b572b9683d6eda3963520aa4e37e328126b319d
divineabagolu <abagoludivine@yahoo.com>
2022-09-12 02:47:25 -0700

    ok

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,19 +15,19 @@ int main(void)
 		for (j = i + 1; j <= 99; j++)
 		{
 			putchar((i / 10) + '0');
-			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
 			putchar(' ');
 			putchar((j / 10) + '0');
 			putchar((j % 10) + '0');
 			if (i == 98 && j == 99)
-				contiune
+			continue; 
 			{
 				putchar(',');
 				putchar(' ');
 			}
 		}
 	}
-	putchar('\n')
+	putchar('\n');
 	return (0);
 }
 
