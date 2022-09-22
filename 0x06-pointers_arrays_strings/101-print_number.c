8f5107de2f7828dcecf40d2aa1f68e98909fd44b
divineabagolu <abagoludivine@yahoo.com>
2022-09-22 07:19:56 -0700

    ok

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,7 +2,7 @@
 
 /**
  * print_number - print an integer, without using long, arrays, or pointers
- *@n: number to be printed
+ * @n: number to be printed
 */
 
 void print_number(int n)
@@ -30,7 +30,6 @@ void print_number(int n)
 			_putchar(digit + '0');
 			positive = (positive - (tens * digit));
 			tens /= 10;
-
 		}
 	}
 }
