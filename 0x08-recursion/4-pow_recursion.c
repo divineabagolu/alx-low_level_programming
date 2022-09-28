4e00dd311e30e97638a838523847ae4637eabba4
divineabagolu <abagoludivine@yahoo.com>
2022-09-28 08:08:05 -0700

    vine

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
- * _pow_recursion - returns the value of x raised to the power of y
- * @x: number to be used
- * @y: exponent to be used
+ * factorial - returns the factorial of a number
+ * @n: number to return the factorial from
  *
- * Return: x ^ y
+ * Return: factorial of n
  */
-int _pow_recursion(int x' int y)
+int factorial(int n)
 {
-	if (y < 0)
+	if (n < 0)
 		return (-1);
-	else if (y === 0)
+	if (n == 0)
 		return (1);
-	return (x * _pow_recursion(x, y - 1));
+	return (n * factorial(n - 1));
 }
